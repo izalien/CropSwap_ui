dc16a48a13cfb5165d42fc8bb8e489a8ae285493
izalien <izaliendecker@gmail.com>
2025-04-12 12:23:42 -0400

    changed modal styling and tweaked crop history colors

--- app/(tabs)/myFields.tsx
+++ app/(tabs)/myFields.tsx
@@ -10,17 +10,18 @@ import LoadingModal from "../components/LoadingModal";
 import BrownButton from "../components/BrownButton";
 import Background from "../components/Background";
 import Title from "../components/Title";
+import CSModal from "../components/CSModal";
 
 export default function MyFields() {
   const [grows, setGrows] = useState(new Array<Grow>());
   const [checkedFields, setCheckedFields] = useState(new Array<boolean>());
   const [year, setYear] = useState(Number);
-  
+
   const getAllCurrentGrows = async () => {
     // fetch grows from API
     try {
-      const season = year || new Date().getFullYear(); 
-      const response = await axios.get('http://localhost:3000/api/grows/getAllCurrent', 
+      const season = year || new Date().getFullYear();
+      const response = await axios.get('http://localhost:3000/api/grows/getAllCurrent',
         { params: { "season": season } }
       );
       setGrows(response.data.data.grows);
@@ -30,7 +31,7 @@ export default function MyFields() {
       console.error(error);
     }
   }
-  
+
   const toggleCheckbox = (index: number) => {
     const updatedCheckedFields = [...checkedFields];
     updatedCheckedFields[index] = !checkedFields[index];
@@ -73,7 +74,7 @@ export default function MyFields() {
   const [size, setSize] = useState(Number);
   const [location, setLocation] = useState("");
   const [fieldCrop, setFieldCrop] = useState({});
-  
+
   const resetValues = () => {
     // reset field values
     setName("");
@@ -94,8 +95,8 @@ export default function MyFields() {
       const grow = {
         season: year,
         field: {
-          name: name, 
-          size: size, 
+          name: name,
+          size: size,
           location: location
         },
         crop: fieldCrop
@@ -105,13 +106,13 @@ export default function MyFields() {
           headers: {
             'Content-Type': 'application/json'
           }
-        } );
+        });
         setNewFieldModalVisible(false);
       }
       catch (error) {
         console.error(error);
-      } 
-  
+      }
+
       // refresh list of fields
       getAllFields();
       resetValues();
@@ -138,13 +139,13 @@ export default function MyFields() {
         headers: {
           'Content-Type': 'application/json'
         }
-      } );
+      });
       setExistingFieldModalVisible(false);
     }
     catch (error) {
       console.error(error);
     }
-    
+
     getAllCurrentGrows();
     setExistingFieldModalVisible(false);
   }
@@ -153,9 +154,9 @@ export default function MyFields() {
   const [crops, setCrops] = useState(new Array<Crop>());
   const [loading, setLoading] = useState(true);
   const [years, setYears] = useState(new Array());
-  
+
   useEffect(() => {
-    getCrops().then((allCrops) => setCrops([{id: null, name: "none"}, ...allCrops]));
+    getCrops().then((allCrops) => setCrops([{ id: null, name: "none" }, ...allCrops]));
 
     if (year === 0) setYear(new Date().getFullYear());
 
@@ -184,143 +185,114 @@ export default function MyFields() {
       <View className="h-full">
         <View className="flex-row h-10 align-middle">
           <Text className="color-amber-100 text-xl m-3">Select year:</Text>
-          <Dropdown value={year} labelField={"value"} valueField={"value"} data={years} style={{ borderColor: '#78350f', borderWidth: 2, padding: 8, borderRadius: 6, backgroundColor: '#fffbeb'}} itemTextStyle={{fontSize: 14}} selectedTextStyle={{fontSize: 14}} onChange={event => setYear(event.value)}/>   
+          <Dropdown value={year} labelField={"value"} valueField={"value"} data={years} style={{ borderColor: '#78350f', borderWidth: 2, padding: 8, borderRadius: 6, backgroundColor: '#fffbeb' }} itemTextStyle={{ fontSize: 14 }} selectedTextStyle={{ fontSize: 14 }} onChange={event => setYear(event.value)} />
         </View>
-        {showRemove && 
+        {showRemove &&
           <Text className="color-amber-50 text-3xl">Select fields for removal</Text>
         }
         {grows.length === 0 ? (
           <Text className="color-amber-50 text-3xl">No fields found.</Text>
         ) : (
-        <FlatList
-          numColumns={3}
-          data={grows}
-          renderItem={({item, index}) => (
-            <View className="bg-amber-100 p-5 m-5 rounded-xl">
-              <View className="flex-row items-center">
-                {showRemove && 
-                  <CheckBox
-                    checked={checkedFields[index]}
-                    onPress={() => {toggleCheckbox(index)}}
-                    iconType="material-community"
-                    checkedIcon="checkbox-marked"
-                    uncheckedIcon="checkbox-blank-outline"
-                    checkedColor='#3c6300'
-                    size={32}
-                    containerStyle={{padding: 0, marginTop: 0, marginLeft: 0, marginRight: 0, paddingBottom: 3}}
-                  />
-                }
-                <Text className="color-amber-950 text-3xl font-serif font-semibold mb-2">{item.field.name.toUpperCase()}</Text>
+        <View className="w-full">
+          <FlatList
+            numColumns={3}
+            data={grows}
+            renderItem={({ item, index }) => (
+              <View className="bg-amber-100 p-5 m-5 rounded-xl">
+                <View className="flex-row items-center">
+                  {showRemove &&
+                    <CheckBox
+                      checked={checkedFields[index]}
+                      onPress={() => { toggleCheckbox(index) }}
+                      iconType="material-community"
+                      checkedIcon="checkbox-marked"
+                      uncheckedIcon="checkbox-blank-outline"
+                      checkedColor='#3c6300'
+                      size={32}
+                      containerStyle={{ padding: 0, marginTop: 0, marginLeft: 0, marginRight: 0, paddingBottom: 3 }}
+                    />
+                  }
+                  <Text className="color-amber-950 text-3xl font-serif font-semibold mb-2">{item.field.name.toUpperCase()}</Text>
+                </View>
+                <View className="flex-row w-full justify-between">
+                  <Text className="color-amber-900 text-xl font-semibold me-5">Size:</Text>
+                  <Text className="color-amber-900 text-xl">{item.field.size} acres</Text>
+                </View>
+                <View className="flex-row w-full justify-between">
+                  <Text className="color-amber-900 text-xl font-semibold me-5">Location:</Text>
+                  <Text className="color-amber-900 text-xl">{item.field.location}</Text>
+                </View>
+                <View className="flex-row w-full justify-between">
+                  <Text className="color-amber-900 text-xl font-semibold me-5">Crop:</Text>
+                  <Text className="color-amber-900 text-xl">{item.crop.name}</Text>
+                </View>
               </View>
-              <View className="flex-row w-full justify-between">
-                <Text className="color-amber-900 text-xl font-semibold me-5">Size:</Text>
-                <Text className="color-amber-900 text-xl">{item.field.size} acres</Text>
-              </View>
-              <View className="flex-row w-full justify-between">
-                <Text className="color-amber-900 text-xl font-semibold me-5">Location:</Text>
-                <Text className="color-amber-900 text-xl">{item.field.location}</Text>
-              </View>
-              <View className="flex-row w-full justify-between">
-                <Text className="color-amber-900 text-xl font-semibold me-5">Crop:</Text>
-                <Text className="color-amber-900 text-xl">{item.crop.name}</Text>
-              </View>
-            </View>
-          )}
-          keyExtractor={(item) => item._id}
-        />)}
+            )}
+            keyExtractor={(item) => item._id}
+          />
+        </View>)}
       </View>
-      {showRemove ?  
+      {showRemove ?
         <View className="bg-lime-900 sticky bottom-0 p-5 flex-row justify-center w-full">
-          <BrownButton onPress={() => {removeFields()}} title="Remove Selected"/>
+          <BrownButton onPress={() => { removeFields() }} title="Remove Selected" />
           <View className="mx-5">
-            <Button title="Cancel" onPress={() => {setShowRemove(false)}} color={'#9f0712'}/>
+            <Button title="Cancel" onPress={() => { setShowRemove(false) }} color={'#9f0712'} />
           </View>
         </View>
         :
         <View className="bg-lime-800 sticky bottom-0 p-5 flex-row justify-center w-full">
           <View className="w-40 mx-5">
-            <BrownButton title="Add Existing Field" onPress={() => {getAllNonCurrentFields()}}/>
+            <BrownButton title="Add Existing Field" onPress={() => { getAllNonCurrentFields() }} />
           </View>
           <View className="w-40 mx-5">
-            <BrownButton title="Add New Field" onPress={() => {setNewFieldModalVisible(true)}}/>
+            <BrownButton title="Add New Field" onPress={() => { setNewFieldModalVisible(true) }} />
           </View>
           <View className="w-40 mx-5">
-            <BrownButton title="Remove Field" onPress={() => setShowRemove(true)}/>
+            <BrownButton title="Remove Field" onPress={() => setShowRemove(true)} />
           </View>
         </View>
       }
-      <Modal 
+      <CSModal
         isVisible={newFieldModalVisible}
-        animationIn="slideInUp"
-        backdropColor={'#78350f'}
-        backdropOpacity={0.50}
+        crops={crops}
+        submit={submitNewField}
+        cancel={setNewFieldModalVisible}
+        header="Add New Field"
+        setFieldCrop={setFieldCrop}
       >
-        <View className="flex place-content-center h-screen items-center">
-          <View className="bg-amber-100 rounded-xl shadow-md">
-            <Text className="font-serif color-amber-50 text-xl bg-amber-900 rounded-t-xl p-5 font-semibold text-center mb-5 border-amber-100 border-2">Add New Field</Text>
-            <View className="flex-row py-2 px-5 items-center">
-              <Text className="color-amber-950 font-semibold w-20">Field Name:</Text>
-              <TextInput className="bg-amber-50 h-10 border-amber-900 border-2 rounded-md p-2" placeholder="Field Name" placeholderTextColor={"#78350fbf"} onChange={event => setName(event.nativeEvent.text)}/>
-            </View>
-            <View className="flex-row py-2 px-5 items-center">
-              <Text className="color-amber-950 font-semibold w-20">Field Size:</Text>
-              <TextInput keyboardType="numeric" className="bg-amber-50 h-10 border-amber-900 border-2 rounded-md p-2" placeholder="0" placeholderTextColor={"#78350fbf"} onChange={event => setSize(Number(event.nativeEvent.text))}/>
-            </View>
-            <View className="flex-row py-2 px-5 items-center">
-              <Text className="color-amber-950 font-semibold w-20">Location Description:</Text>
-              <TextInput className="bg-amber-50 h-10 border-amber-900 border-2 rounded-md p-2" placeholder="Location" placeholderTextColor={"#78350fbf"} onChange={event => setLocation(event.nativeEvent.text)}/>
-            </View>
-            <View className="flex-row py-2 px-5 items-center">
-              <Text className="color-amber-950 font-semibold w-20">Crop:</Text>
-              <Dropdown value={crops[0]} labelField={"name"} valueField={"id"} data={crops} placeholder="Crop" placeholderStyle={{ color: "#78350fbf", fontSize: 14 }} style={{ width: '69%', borderColor: '#78350f', borderWidth: 2, padding: 8, borderRadius: 6, backgroundColor: '#fffbeb' }} itemTextStyle={{fontSize: 14}} selectedTextStyle={{fontSize: 14}} onChange={event => setFieldCrop(event)}/>
-            </View>
-            <View className="flex-row p-5 justify-center">
-              <View className="mx-5">
-                <Button title="Submit" onPress={() => {submitNewField()}} color={'#3c6300'}/>
-              </View>
-              <View className="mx-5">
-                <Button title="Cancel" onPress={() => {setNewFieldModalVisible(false)}} color={'#9f0712'}/>
-              </View>
-            </View>
-          </View>
+        <View className="flex-row py-2 px-5 items-center">
+          <Text className="color-amber-950 font-semibold w-20">Field Name:</Text>
+          <TextInput className="bg-amber-50 h-10 border-amber-900 border-2 rounded-md p-2" placeholder="Field Name" placeholderTextColor={"#78350fbf"} onChange={event => setName(event.nativeEvent.text)} />
         </View>
-      </Modal>
-      <Modal 
+        <View className="flex-row py-2 px-5 items-center">
+          <Text className="color-amber-950 font-semibold w-20">Field Size:</Text>
+          <TextInput keyboardType="numeric" className="bg-amber-50 h-10 border-amber-900 border-2 rounded-md p-2" placeholder="0" placeholderTextColor={"#78350fbf"} onChange={event => setSize(Number(event.nativeEvent.text))} />
+        </View>
+        <View className="flex-row py-2 px-5 items-center">
+          <Text className="color-amber-950 font-semibold w-20">Location Description:</Text>
+          <TextInput className="bg-amber-50 h-10 border-amber-900 border-2 rounded-md p-2" placeholder="Location" placeholderTextColor={"#78350fbf"} onChange={event => setLocation(event.nativeEvent.text)} />
+        </View>
+      </CSModal>
+      <CSModal
         isVisible={existingFieldModalVisible}
-        animationIn="slideInUp"
-        backdropColor={'#78350f'}
-        backdropOpacity={0.50}
-      >
-        <View className="flex place-content-center h-screen items-center">
-          <View className="bg-amber-100 rounded-xl shadow-md">
-            <Text className="font-serif color-amber-50 text-xl bg-amber-900 rounded-t-xl p-5 font-semibold text-center mb-5 border-amber-100 border-2">Add Existing Field</Text>
-            <View className="flex-row py-2 px-5 items-center">
-              <Text className="color-amber-950 font-semibold w-20">Field Name:</Text>
-              <Dropdown value={nonCurrentFields[0]} labelField={"name"} valueField={"id"} data={nonCurrentFields} placeholder="Field Name" placeholderStyle={{ color: "#78350fbf", fontSize: 14 }} style={{ width: '69%', borderColor: '#78350f', borderWidth: 2, padding: 8, borderRadius: 6, backgroundColor: '#fffbeb' }} itemTextStyle={{fontSize: 14}} selectedTextStyle={{fontSize: 14}} onChange={event => setSelectedField(event)}/>
-            </View>
-            <View className="flex-row py-2 px-5 items-center">
-              <Text className="color-amber-950 font-semibold w-20">Field Size:</Text>
-              <TextInput keyboardType="numeric" className="bg-amber-50 h-10 border-amber-900 border-2 rounded-md p-2" placeholder="0" placeholderTextColor={"#78350fbf"} editable={false} value={selectedField?.size.toString()}/>
-            </View>
-            <View className="flex-row py-2 px-5 items-center">
-              <Text className="color-amber-950 font-semibold w-20">Location Description:</Text>
-              <TextInput className="bg-amber-50 h-10 border-amber-900 border-2 rounded-md p-2" placeholder="Location" placeholderTextColor={"#78350fbf"}  editable={false} value={selectedField?.location}/>
-            </View>
-            <View className="flex-row py-2 px-5 items-center">
-              <Text className="color-amber-950 font-semibold w-20">Crop:</Text>
-              <Dropdown value={crops[0]} labelField={"name"} valueField={"id"} data={crops} placeholder="Crop" placeholderStyle={{ color: "#78350fbf", fontSize: 14 }} style={{ width: '69%', borderColor: '#78350f', borderWidth: 2, padding: 8, borderRadius: 6, backgroundColor: '#fffbeb' }} itemTextStyle={{fontSize: 14}} selectedTextStyle={{fontSize: 14}} onChange={event => setFieldCrop(event)}/>
-            </View>
-            <View className="flex-row p-5 justify-center">
-              <View className="mx-5">
-                <Button title="Submit" onPress={() => {submitExistingField()}} color={'#3c6300'}/>
-              </View>
-              <View className="mx-5">
-                <Button title="Cancel" onPress={() => {setExistingFieldModalVisible(false)}} color={'#9f0712'}/>
-              </View>
-            </View>
-          </View>
+        crops={crops}
+        submit={submitExistingField}
+        cancel={setExistingFieldModalVisible}
+        header="Add Existing Field"
+        setFieldCrop={setFieldCrop}
+      ><View className="flex-row py-2 px-5 items-center">
+          <Text className="color-amber-950 font-semibold w-20">Field Name:</Text>
+          <Dropdown value={nonCurrentFields[0]} labelField={"name"} valueField={"id"} data={nonCurrentFields} placeholder="Field Name" placeholderStyle={{ color: "#78350fbf", fontSize: 14 }} style={{ width: '69%', borderColor: '#78350f', borderWidth: 2, padding: 8, borderRadius: 6, backgroundColor: '#fffbeb' }} itemTextStyle={{ fontSize: 14 }} selectedTextStyle={{ fontSize: 14 }} onChange={event => setSelectedField(event)} />
         </View>
-      </Modal>
+        <View className="flex-row py-2 px-5 items-center">
+          <Text className="color-amber-950 font-semibold w-20">Field Size:</Text>
+          <TextInput keyboardType="numeric" className="bg-amber-50 h-10 border-amber-900 border-2 rounded-md p-2" placeholder="0" placeholderTextColor={"#78350fbf"} editable={false} value={selectedField?.size.toString()} />
+        </View>
+        <View className="flex-row py-2 px-5 items-center">
+          <Text className="color-amber-950 font-semibold w-20">Location Description:</Text>
+          <TextInput className="bg-amber-50 h-10 border-amber-900 border-2 rounded-md p-2" placeholder="Location" placeholderTextColor={"#78350fbf"} editable={false} value={selectedField?.location} />
+        </View>
+      </CSModal>
       <LoadingModal loading={loading} />
     </Background>
   );
